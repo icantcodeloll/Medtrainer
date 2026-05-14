c46025a24b17c67c2d7b411210a9b89fa0135117
icantcodeloll <adirdran12@gmail.com>
2026-05-14 17:50:40 +1000

    revert

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ if st.sidebar.button("Save Progress", help="Manually save your current progress"
 def get_client():
     return genai.Client(api_key=API_KEYS[st.session_state.key_index])
 
-def call_gemini_with_rotation(prompt, model_to_use, use_search=False):
+def call_gemini_with_rotation(prompt, model_to_use, use_search=False, timeout_per_question=3):
     keys_tried = 0
     
     # Configure Google Search Tool if requested
